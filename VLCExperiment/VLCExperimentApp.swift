f4a23172ee31f763e9ef2b99475789072e598f3a
mkotyak <kotyak.maria@gmail.com>
2023-10-22 11:59:17 +0300

    Initial commit

---
+++ VLCExperiment/VLCExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  VLCExperimentApp.swift
+//  VLCExperiment
+//
+//  Created by Maria Kotyak on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct VLCExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
